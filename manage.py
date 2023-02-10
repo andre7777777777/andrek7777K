8abb2067813e13f5f16e11b3bf24fc75caee3e91
andre7777777777 <123818340+andre7777777777@users.noreply.github.com>
2023-02-10 18:24:41 +0200

    Add files via upload

---
+++ manage.py
@@ -0,0 +1,23 @@
+import openai
+import telebot
+#можете изменить api ключ, но можете оставить этот просто вставив токен бота.
+openai.api_key = "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"
+bot = telebot.TeleBot("TOKEN")
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Как я могу Вам помочь?")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt='Расскажи мне о ' + message.text + '?',
+        max_tokens=2048,
+        n = 1,
+        stop=None,
+        temperature=0.5,
+    )
+    bot.reply_to(message, response["choices"][0]["text"])
+
+bot.polling()
